24ebf8efdbbed18779458448c419df9c90dd5603
Travis CI User <travis@example.org>
2021-03-16 08:52:57 +0000

    refactor: RHICOMPL-1355 presentational TabbedRules from PolicyMultiversionRules
    Created presentational component TabbedRules from PolicyMultiversionRules

---
+++ js/236.912373ed94d874b9363b.js
@@ -0,0 +1,2 @@
+(self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[236],{30236:(n,e,c)=>{var o=c(42980),p=c(21463)((function(n,e,c,p){o(n,e,c,p)}));n.exports=p}}]);
+//# sourceMappingURL=236.912373ed94d874b9363b.js.map
