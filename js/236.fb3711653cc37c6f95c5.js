cccc7c652cbf5dbe4ec4b791be4e4d5925366170
Travis CI User <travis@example.org>
2021-04-19 08:24:14 +0000

    chore: Bump packages (#1143)
    * chore(deps): Update @redhatservices & @patternfly packages
    
    * chore(deps): Updating outdated packages
    
    * chor(deps): Update Apollo dependencies

---
+++ js/236.fb3711653cc37c6f95c5.js
@@ -0,0 +1,1 @@
+(self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[236],{30236:(n,e,c)=>{var o=c(42980),p=c(21463)((function(n,e,c,p){o(n,e,c,p)}));n.exports=p}}]);
