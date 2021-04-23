0fadd2d08aba5da38d59fad48d25eda66cd39a5b
Travis CI User <travis@example.org>
2021-04-23 08:55:59 +0000

    fix(EditPolicy): RHICOMPL-1719 show SSG ver for new tabs

--- js/665.4c701a8f7c090009eb6b.js
+++ js/665.4c701a8f7c090009eb6b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[665],{14665:(e,n,t)=>{"use strict";function c(e,n){return(c=Object.setPrototypeOf||function(e,n){return e.__proto__=n,e})(e,n)}t.d(n,{Z:()=>c})}}]);
-//# sourceMappingURL=../sourcemaps/665.725f5688a4ad30be5189.js.map
+//# sourceMappingURL=../sourcemaps/665.60b6bbaebfc48daa9959.js.map
--- js/788.6611a962d6d35a5e28a6.js
+++ js/788.6611a962d6d35a5e28a6.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[788],{41788:(e,t,o)=>{"use strict";o.d(t,{Z:()=>n});var c=o(14665);function n(e,t){e.prototype=Object.create(t.prototype),e.prototype.constructor=e,(0,c.Z)(e,t)}}}]);
-//# sourceMappingURL=../sourcemaps/788.725f5688a4ad30be5189.js.map
+//# sourceMappingURL=../sourcemaps/788.60b6bbaebfc48daa9959.js.map
