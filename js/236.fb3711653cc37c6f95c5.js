08aa50beb78bd279ce0a0553b678386157067d70
Travis CI User <travis@example.org>
2021-04-27 14:37:59 +0000

    chore(deps): Update @redhat-cloud-services
    * frontend-components -> ^3.1.10
    * frontend-components-inventory-compliance -> ^3.3.0
    * frontend-components-utilities -> ^3.1.2
    * frontend-components-config -> ^4.0.28

--- js/236.fb3711653cc37c6f95c5.js
+++ js/236.fb3711653cc37c6f95c5.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[236],{30236:(n,e,c)=>{var o=c(42980),p=c(21463)((function(n,e,c,p){o(n,e,c,p)}));n.exports=p}}]);
-//# sourceMappingURL=../sourcemaps/236.ef51848bd5368f3bbd54.js.map
+//# sourceMappingURL=../sourcemaps/236.6d38f78d537b3e6c39ba.js.map
--- js/665.4c701a8f7c090009eb6b.js
+++ js/665.4c701a8f7c090009eb6b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[665],{14665:(e,n,t)=>{"use strict";function c(e,n){return(c=Object.setPrototypeOf||function(e,n){return e.__proto__=n,e})(e,n)}t.d(n,{Z:()=>c})}}]);
-//# sourceMappingURL=../sourcemaps/665.ef51848bd5368f3bbd54.js.map
+//# sourceMappingURL=../sourcemaps/665.6d38f78d537b3e6c39ba.js.map
