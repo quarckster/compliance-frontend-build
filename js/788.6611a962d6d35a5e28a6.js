f6f18decfd2a1f4d58f6c50a2d98711106578108
Travis CI User <travis@example.org>
2021-04-27 14:41:51 +0000

    chore(deps): Update @redhat-cloud-services
    * frontend-components -> ^3.1.10
    * frontend-components-inventory-compliance -> ^3.3.0
    * frontend-components-utilities -> ^3.1.2
    * frontend-components-config -> ^4.0.28

--- js/788.6611a962d6d35a5e28a6.js
+++ js/788.6611a962d6d35a5e28a6.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[788],{41788:(e,t,o)=>{"use strict";o.d(t,{Z:()=>n});var c=o(14665);function n(e,t){e.prototype=Object.create(t.prototype),e.prototype.constructor=e,(0,c.Z)(e,t)}}}]);
-//# sourceMappingURL=../sourcemaps/788.90f5cd74fb5be1789a6b.js.map
+//# sourceMappingURL=../sourcemaps/788.8539e236ba577ef85f3e.js.map
