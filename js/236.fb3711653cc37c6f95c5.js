70fe6f5145b5b0974cdfd160540bc1bee08d80c5
Travis CI User <travis@example.org>
2021-04-27 08:16:18 +0000

    fix(EditPolicy): RHICOMPL-1742 save button disabled on empty systems
    The updatedPolicy has been set more than once in EditPolicyForm whithin
    three different useEffect callbacks.  Because the effects did not have it
    as a dependency (for triggering reasons) they do not get the updated
    content from the previous useEffect.  Since these useEffects can fire
    all at the same time, one would not get the update from the other.  This
    fixes this by splitting it to three separate state variables.

--- js/236.fb3711653cc37c6f95c5.js
+++ js/236.fb3711653cc37c6f95c5.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[236],{30236:(n,e,c)=>{var o=c(42980),p=c(21463)((function(n,e,c,p){o(n,e,c,p)}));n.exports=p}}]);
-//# sourceMappingURL=../sourcemaps/236.a840778b29012dfaa628.js.map
+//# sourceMappingURL=../sourcemaps/236.662f5b7a29d52375fe37.js.map
