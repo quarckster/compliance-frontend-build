636a914260520ecd5de03169b0ff3a5863c49bee
Travis CI User <travis@example.org>
2021-04-27 08:42:28 +0000

    fix(EditPolicy): RHICOMPL-1755 Keep rule selections when systems change (#1187)
    Before, when updating the selected systems (not even adding or removing new
    OS tabs), the rule selections were reset to the original policy's rule
    selections. This change keeps the rule selection reset only when the
    modal is first loaded (`useEffect(..., [policy])`).
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- js/665.4c701a8f7c090009eb6b.js
+++ js/665.4c701a8f7c090009eb6b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[665],{14665:(e,n,t)=>{"use strict";function c(e,n){return(c=Object.setPrototypeOf||function(e,n){return e.__proto__=n,e})(e,n)}t.d(n,{Z:()=>c})}}]);
-//# sourceMappingURL=../sourcemaps/665.ebf5663335942094818a.js.map
+//# sourceMappingURL=../sourcemaps/665.d0a18ac1fcace9826a69.js.map
