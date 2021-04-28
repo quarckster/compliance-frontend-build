768552c37610a176e4128248d8b9a6f894a46af3
Travis CI User <travis@example.org>
2021-04-28 09:41:59 +0000

    fix(PolicyDetails): minor typo s/rule/rules/ (#1190)
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- js/236.fb3711653cc37c6f95c5.js
+++ js/236.fb3711653cc37c6f95c5.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[236],{30236:(n,e,c)=>{var o=c(42980),p=c(21463)((function(n,e,c,p){o(n,e,c,p)}));n.exports=p}}]);
-//# sourceMappingURL=../sourcemaps/236.d7c5c52661866458169c.js.map
+//# sourceMappingURL=../sourcemaps/236.6e137c643bfa8b274424.js.map
--- js/665.4c701a8f7c090009eb6b.js
+++ js/665.4c701a8f7c090009eb6b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[665],{14665:(e,n,t)=>{"use strict";function c(e,n){return(c=Object.setPrototypeOf||function(e,n){return e.__proto__=n,e})(e,n)}t.d(n,{Z:()=>c})}}]);
-//# sourceMappingURL=../sourcemaps/665.d7c5c52661866458169c.js.map
+//# sourceMappingURL=../sourcemaps/665.6e137c643bfa8b274424.js.map
