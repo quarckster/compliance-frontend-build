65be88af3eec3cf49242a381db89e39330400a32
Travis CI User <travis@example.org>
2021-04-29 00:00:55 +0000

    Bump @redhat-cloud-services/frontend-components-inventory-compliance (#613)
    Bumps [@redhat-cloud-services/frontend-components-inventory-compliance](https://github.com/RedHatInsights/frontend-components) from 2.0.3 to 2.0.4.
    - [Release notes](https://github.com/RedHatInsights/frontend-components/releases)
    - [Commits](https://github.com/RedHatInsights/frontend-components/commits)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>

--- js/788.6611a962d6d35a5e28a6.js
+++ js/788.6611a962d6d35a5e28a6.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[788],{41788:(e,t,o)=>{"use strict";o.d(t,{Z:()=>n});var c=o(14665);function n(e,t){e.prototype=Object.create(t.prototype),e.prototype.constructor=e,(0,c.Z)(e,t)}}}]);
-//# sourceMappingURL=../sourcemaps/788.2b540898c2c28b0409a9.js.map
+//# sourceMappingURL=../sourcemaps/788.9ad02f076381fc7fff06.js.map
