34924942e634b0c2dd3b906882c50a7e6ed203a2
Travis CI User <travis@example.org>
2021-04-29 06:24:25 +0000

    Merge pull request #1194 from bastilian/RHICOMPL-1476-followup-followup
    fix(ReportDetails): RHICOMPL-1476 - Add OS minor version filter

--- js/236.fb3711653cc37c6f95c5.js
+++ js/236.fb3711653cc37c6f95c5.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[236],{30236:(n,e,c)=>{var o=c(42980),p=c(21463)((function(n,e,c,p){o(n,e,c,p)}));n.exports=p}}]);
-//# sourceMappingURL=../sourcemaps/236.6e137c643bfa8b274424.js.map
+//# sourceMappingURL=../sourcemaps/236.d24dc69f8d0efc08c2a2.js.map
