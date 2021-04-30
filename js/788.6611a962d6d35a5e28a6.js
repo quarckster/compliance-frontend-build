0541112def6d5812368707f0d26bbfb0e28f28e3
Travis CI User <travis@example.org>
2021-04-30 15:12:50 +0000

    fix(EditPolicySystemsTab): RHICOMPL-1697 new rules alert
    The new rules alert should only show when new tabs appear. This PR fixes
    an issue where the alert appeared for new systems, despite not having
    a valid policy type due to the OS version. This solution highlights the
    performance issue with the rules tab requesting all benchmark rules.
    This request is required to determine if the newly added system OS
    versions have this policy type, so the alert is quite sluggish, on the
    order of 1-2 seconds, to update when new systems are selected. I'm
    hopeful we'll optimize the rules tab queries, which will also fix this
    sluggish alert.
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- js/788.6611a962d6d35a5e28a6.js
+++ js/788.6611a962d6d35a5e28a6.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[788],{41788:(e,t,o)=>{"use strict";o.d(t,{Z:()=>n});var c=o(14665);function n(e,t){e.prototype=Object.create(t.prototype),e.prototype.constructor=e,(0,c.Z)(e,t)}}}]);
-//# sourceMappingURL=../sourcemaps/788.545f4b33464d8c9eb7c3.js.map
+//# sourceMappingURL=../sourcemaps/788.6cabb56f5516f4d05f38.js.map
