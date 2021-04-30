06245bcd290373d213d52150b19590c779c2ef2c
Travis CI User <travis@example.org>
2021-04-30 08:07:09 +0000

    feat(spandx): Disable goodguy by default (#1197)

--- js/665.4c701a8f7c090009eb6b.js
+++ js/665.4c701a8f7c090009eb6b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[665],{14665:(e,n,t)=>{"use strict";function c(e,n){return(c=Object.setPrototypeOf||function(e,n){return e.__proto__=n,e})(e,n)}t.d(n,{Z:()=>c})}}]);
-//# sourceMappingURL=../sourcemaps/665.d0a18ac1fcace9826a69.js.map
+//# sourceMappingURL=../sourcemaps/665.545f4b33464d8c9eb7c3.js.map
