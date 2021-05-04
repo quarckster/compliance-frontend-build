f83e1ed62f60fe4f9d9a6ac3e381024f6043b32f
Travis CI User <travis@example.org>
2021-05-04 00:04:08 +0000

    Bump @redhat-cloud-services/frontend-components-inventory-compliance (#613)
    Bumps [@redhat-cloud-services/frontend-components-inventory-compliance](https://github.com/RedHatInsights/frontend-components) from 2.0.3 to 2.0.4.
    - [Release notes](https://github.com/RedHatInsights/frontend-components/releases)
    - [Commits](https://github.com/RedHatInsights/frontend-components/commits)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>

--- js/788.6611a962d6d35a5e28a6.js
+++ js/788.6611a962d6d35a5e28a6.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[788],{41788:(e,t,o)=>{"use strict";o.d(t,{Z:()=>n});var c=o(14665);function n(e,t){e.prototype=Object.create(t.prototype),e.prototype.constructor=e,(0,c.Z)(e,t)}}}]);
-//# sourceMappingURL=../sourcemaps/788.daf684e158d32ab5bf9b.js.map
+//# sourceMappingURL=../sourcemaps/788.efc270e3ecd19d9f1f1a.js.map
