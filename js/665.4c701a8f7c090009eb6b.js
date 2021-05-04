c9104685f7725f7af7e256d99b4dba9097224419
Travis CI User <travis@example.org>
2021-05-04 14:11:47 +0000

    fix(CreatePolicy): RHICOMPL-1787 show SSG version in Rules step

--- js/665.4c701a8f7c090009eb6b.js
+++ js/665.4c701a8f7c090009eb6b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[665],{14665:(e,n,t)=>{"use strict";function c(e,n){return(c=Object.setPrototypeOf||function(e,n){return e.__proto__=n,e})(e,n)}t.d(n,{Z:()=>c})}}]);
-//# sourceMappingURL=../sourcemaps/665.4b73a1ae91268a4514b2.js.map
+//# sourceMappingURL=../sourcemaps/665.306244e11ca3ab737ea5.js.map
