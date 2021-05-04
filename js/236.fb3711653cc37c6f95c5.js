4b13349e5cac482fdd8b1e8e6075a16378719038
Travis CI User <travis@example.org>
2021-05-04 15:24:11 +0000

    fix: RHICOMPL-1701 Adjust wizard and modal sizes
    Remove all specific pixel values and rely on isLarge in the modal.
    Additionally, set the modal position to 'top' to tie it to the top
    of the screen so the tabs don't change absolute position on the
    screen as the user switches tabs.
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- js/236.fb3711653cc37c6f95c5.js
+++ js/236.fb3711653cc37c6f95c5.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[236],{30236:(n,e,c)=>{var o=c(42980),p=c(21463)((function(n,e,c,p){o(n,e,c,p)}));n.exports=p}}]);
-//# sourceMappingURL=../sourcemaps/236.2e288316f9732826aeac.js.map
+//# sourceMappingURL=../sourcemaps/236.247ca0ae2f2574e46321.js.map
