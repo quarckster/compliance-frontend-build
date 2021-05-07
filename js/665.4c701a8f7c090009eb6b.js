b3869b2f4ee7fe300f2f36f5f6a17eee95deb181
Travis CI User <travis@example.org>
2021-05-07 12:46:06 +0000

    refactor(ProfileThresholdField): Remove unused showTitle prop
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- js/665.4c701a8f7c090009eb6b.js
+++ js/665.4c701a8f7c090009eb6b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[665],{14665:(e,n,t)=>{"use strict";function c(e,n){return(c=Object.setPrototypeOf||function(e,n){return e.__proto__=n,e})(e,n)}t.d(n,{Z:()=>c})}}]);
-//# sourceMappingURL=../sourcemaps/665.b031db1f5a6035dd10d4.js.map
+//# sourceMappingURL=../sourcemaps/665.80ba491302a4291eb167.js.map
