b8706f63740cd3ebe19c8b057896aa6ab8dbffdc
Travis CI User <travis@example.org>
2021-05-10 12:56:01 +0000

    fix(EditPolicy): RHICOMPL-1701 Reduce minHeight to 350px
    This fixes the button placement on screens smaller than 768px. The issue
    will still be present on screens smaller than 350px, but that should be
    much less common.
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- js/665.4c701a8f7c090009eb6b.js
+++ js/665.4c701a8f7c090009eb6b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[665],{14665:(e,n,t)=>{"use strict";function c(e,n){return(c=Object.setPrototypeOf||function(e,n){return e.__proto__=n,e})(e,n)}t.d(n,{Z:()=>c})}}]);
-//# sourceMappingURL=../sourcemaps/665.10f1948049ab3aaff830.js.map
+//# sourceMappingURL=../sourcemaps/665.452485425f0eeebb96b5.js.map
