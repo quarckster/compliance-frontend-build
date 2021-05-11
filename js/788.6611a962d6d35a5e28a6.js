552b0fb1f696984b7444cb33d77546dd3692c20d
Travis CI User <travis@example.org>
2021-05-11 09:48:31 +0000

    fix(SystemsTable): RHICOMPL-1254 - Remove dependencies on InventoryStore (#1206)

--- js/788.6611a962d6d35a5e28a6.js
+++ js/788.6611a962d6d35a5e28a6.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[788],{41788:(e,t,o)=>{"use strict";o.d(t,{Z:()=>n});var c=o(14665);function n(e,t){e.prototype=Object.create(t.prototype),e.prototype.constructor=e,(0,c.Z)(e,t)}}}]);
-//# sourceMappingURL=../sourcemaps/788.f4fd62b77c2139d01af0.js.map
+//# sourceMappingURL=../sourcemaps/788.2066befefe4a0ae93eb0.js.map
