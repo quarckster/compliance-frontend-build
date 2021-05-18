5664f4ff40712479925c905c5f3b7909926746eb
Travis CI User <travis@example.org>
2021-05-18 00:13:28 +0000

    Bump @redhat-cloud-services/frontend-components-inventory-compliance (#613)
    Bumps [@redhat-cloud-services/frontend-components-inventory-compliance](https://github.com/RedHatInsights/frontend-components) from 2.0.3 to 2.0.4.
    - [Release notes](https://github.com/RedHatInsights/frontend-components/releases)
    - [Commits](https://github.com/RedHatInsights/frontend-components/commits)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>

--- js/236.fb3711653cc37c6f95c5.js
+++ js/236.fb3711653cc37c6f95c5.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[236],{30236:(n,e,c)=>{var o=c(42980),p=c(21463)((function(n,e,c,p){o(n,e,c,p)}));n.exports=p}}]);
-//# sourceMappingURL=../sourcemaps/236.87ef1132228b4d363c40.js.map
+//# sourceMappingURL=../sourcemaps/236.43ec86f1470b2cff9846.js.map
--- js/788.6611a962d6d35a5e28a6.js
+++ js/788.6611a962d6d35a5e28a6.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[788],{41788:(e,t,o)=>{"use strict";o.d(t,{Z:()=>n});var c=o(14665);function n(e,t){e.prototype=Object.create(t.prototype),e.prototype.constructor=e,(0,c.Z)(e,t)}}}]);
-//# sourceMappingURL=../sourcemaps/788.87ef1132228b4d363c40.js.map
+//# sourceMappingURL=../sourcemaps/788.43ec86f1470b2cff9846.js.map
