17e2e14d3d6ed080c74758561ca6e9242e1a2793
Travis CI User <travis@example.org>
2021-05-24 16:12:28 +0000

    chore(deps): RHICOMPL-744 bump frontend-components-inventory-compliance (#1217)

--- js/236.fb3711653cc37c6f95c5.js
+++ js/236.fb3711653cc37c6f95c5.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[236],{30236:(n,e,c)=>{var o=c(42980),p=c(21463)((function(n,e,c,p){o(n,e,c,p)}));n.exports=p}}]);
-//# sourceMappingURL=../sourcemaps/236.96ab89a4a0a5c490157a.js.map
+//# sourceMappingURL=../sourcemaps/236.30c86cd1dcca71b68072.js.map
