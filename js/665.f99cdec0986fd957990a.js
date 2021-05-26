6c0b2f83dba8bb6eb72ca1213a403c17d96fe378
Travis CI User <travis@example.org>
2021-05-26 13:56:14 +0000

    feat: RHICOMPL-1859 add OUIA IDs (#926)
    Orignal Author: Mayuri Lahane <mlahane@redhat.com>

--- js/665.f99cdec0986fd957990a.js
+++ js/665.f99cdec0986fd957990a.js
@@ -1,2 +1,2 @@
 (self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[665],{14665:(e,n,t)=>{"use strict";function c(e,n){return(c=Object.setPrototypeOf||function(e,n){return e.__proto__=n,e})(e,n)}t.d(n,{Z:()=>c})}}]);
-//# sourceMappingURL=../sourcemaps/665.f84e5974fcfb1635fb48.js.map
+//# sourceMappingURL=../sourcemaps/665.240dec9142bb1c60d7e6.js.map
