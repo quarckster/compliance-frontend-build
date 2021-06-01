b82314407e5b68a70c77b64fd6a9baa955a4fe92
Travis CI User <travis@example.org>
2021-06-01 14:11:46 +0000

    feat(RulesTable): RHICOMPL-942 - Implement rules table using hooks (#1208)
    * feat(hooks): Introduce TableTools
    
    * feat(Components): Build RulesTable with TableTools
    
    * feat(TabbedRules): RHICOMPL-942 - Use new RulesTable component
    
    * fix: Address issue when deselecting on last page
    
    * fix: Remove conditional hook calls
    
    * fix: Memoize filterConfigBuilder
    
    * fix: Correct lock file after rebase
    
    * fix: Use proper allCount for Select/Unselect All
    
    * fix: Set and update selected ids correctly
    
    * fix: Prevent page 0 on negative setPage
    
    * fix: Set preselected default differently
    
    * fix: Add nowrap for switch and bulk select
    
    * fix: Set pagination properly
    
    * fix: Correct bulk select with filtered results

--- js/156.64e68bfbe01c6a65085d.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[156],{96156:(e,n,c)=>{"use strict";function r(e,n,c){return n in e?Object.defineProperty(e,n,{value:c,enumerable:!0,configurable:!0,writable:!0}):e[n]=c,e}c.d(n,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/156.c47db7fedc5f0750861d.js.map
--- js/665.f99cdec0986fd957990a.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[665],{14665:(e,n,t)=>{"use strict";function c(e,n){return(c=Object.setPrototypeOf||function(e,n){return e.__proto__=n,e})(e,n)}t.d(n,{Z:()=>c})}}]);
-//# sourceMappingURL=../sourcemaps/665.c47db7fedc5f0750861d.js.map
