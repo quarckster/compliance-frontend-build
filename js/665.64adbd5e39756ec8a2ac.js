9ea973f97e08eec6cba314784df1805886dfec53
Travis CI User <travis@example.org>
2021-06-08 17:56:46 +0000

    fix(FilterConfig): RHICOMPL-1905 - Only call buildFilterString without array (#1269)

--- js/665.64adbd5e39756ec8a2ac.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkcompliance_frontend=self.webpackChunkcompliance_frontend||[]).push([[665],{14665:(e,n,t)=>{"use strict";function c(e,n){return(c=Object.setPrototypeOf||function(e,n){return e.__proto__=n,e})(e,n)}t.d(n,{Z:()=>c})}}]);
